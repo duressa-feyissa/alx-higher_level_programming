54432429e7e8cde0fcd901fdea7a540f7518c5bc
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-06-27 05:07:33 -0700

    alx

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -13,9 +13,6 @@ int check_cycle(listint_t *list)
 	if (list == NULL)
 		return (0);
 
-	if (list->next == NULL)
-		return (0);
-
 	slow = list->next;
 	fast = list->next->next;
 	while (slow && fast)
