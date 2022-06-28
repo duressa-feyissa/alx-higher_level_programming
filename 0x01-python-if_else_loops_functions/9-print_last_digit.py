23734326bd7e1db8935530fd11ffec589451b28b
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-06-28 04:01:10 -0700

    alx

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+def print_last_digit(number):
+    """ A function that prints the last digit of a number.
+
+    Args:
+        number: value
+
+    Returns:
+        last digit
+    """
+    if number >= 0:
+        last_digit = number % 10
+    else:
+        last_digit = (number * -1) % 10
+    print(last_digit, end="")
+    return last_digit
