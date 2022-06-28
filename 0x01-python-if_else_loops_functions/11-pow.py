f52904b9d922799cb1c754402d19ef898fc32ecf
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-06-28 04:38:36 -0700

    alx

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """My power function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a ** b
+    """
+    return (a ** b)
