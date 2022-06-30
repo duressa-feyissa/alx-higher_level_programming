82e3c9a207437be36226d132b60d09d15b948b5c
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-06-30 04:34:47 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == '__main__':
     argc = len(argv) - 1
     if argc == 0:
         print("{0:d} arguments.".format(argc))
+    elif argc == 1:
+        print("{0:d} argument:".format(argc))
+        print("{0:d}: {1:s}".format(1, argv[1]))
     else:
         print("{0:d} arguments:".format(argc))
         for i in range(1, argc + 1):
