703a1eb1a33c63366ba52baad9c73e7fa22b2d51
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-03 08:08:27 -0700

    replace

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0:
-        return None
+        return my_list
     new_list = list(my_list)
     for i in range(len(new_list)):
         if i == idx:
             new_list[i] = element
             return new_list
     else:
-        return None
+        return my_list
