996e283f254fc8f40d788fb8abc48f0803dc0902
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-03 08:30:46 -0700

    tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    new_tuple = ""
+    new_tuple = (0, None)
     if sentence is None:
-        return new_tuple[0]
+        return new_tuple
     new_tuple = (len(sentence), sentence[0])
     return new_tuple
