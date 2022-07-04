49b3fb841e94656d8e330801b2a57e2fa0f27eb9
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-04 02:39:38 -0700

    replace

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_str = ""
-    for i in my_string:
-        if i != 'c' and i != 'C':
-            new_str += i
-    return new_str
+    new_string = my_string.translate({ord('c'):None})
+    new_string = new_string.translate({ord('C'):None})
+    return new_string
