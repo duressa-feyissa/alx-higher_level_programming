dfd3f47ce34cf312104907dbdf6bcb8432a1f1d4
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-06 03:47:28 -0700

    replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new = list(my_list)
+    for i in range(len(my_list)):
+        if new[i] is search:
+            new[i] = replace
+    return new
