cdfe5a3e6e980e44444b00096b9b19c2708ea082
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-18 14:01:44 -0700

    alx

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-Square = __import__('0-square').Square
+safe_print_list = __import__('0-safe_print_list').safe_print_list
 
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
