29fe2fe52eaa601ff7f0b040a5ec760723c56aac
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-18 14:32:24 -0700

    alx

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as er:
+    except (ValueError, TypeError) as er:
         print("Exception: {}".format(er), file=sys.stderr)
         return False
