15cf71b890c16f7e01a89fd2ea443f1526f125b5
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-18 14:37:42 -0700

    alx

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        return fct(args[0], args[1])
+        return fct(*args)
     except Exception as er:
         print("Exception: {}".format(er), file=sys.stderr)
         return None
