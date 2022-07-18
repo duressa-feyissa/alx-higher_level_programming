822ae03198454737db1f73324ff82838d326a003
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-18 14:11:59 -0700

    alx

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     hold = True
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except (ValueError, TypeError):
         hold = False
     return hold
