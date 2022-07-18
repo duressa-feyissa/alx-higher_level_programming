1e4b2b97c3a761eff10a0add1c3850145d8a47e5
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-18 14:46:08 -0700

    alx

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -5,9 +5,9 @@ def magic_calculation(a, b):
         try:
             if i > a:
                 raise Exception('Too far')
-            else:
-                result += a ** b / i
-        except:
+
+            result += a ** b / i
+        except Exception:
             result = a + b
             break
     return result
