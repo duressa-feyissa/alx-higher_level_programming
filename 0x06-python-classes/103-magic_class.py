13381b1cc2076980896b7209d8ed3a32d9054740
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-19 16:10:20 -0700

    alx

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -14,7 +14,7 @@ class MagicClass:
 
    def area(self):
       """Calculaes the area of the circle"""
-      return self.__radius ** 2 * math.pi
+      return (self.__radius ** 2) * math.pi
 
    def circumference(self):
       """Calculates the circumference of the circle"""
