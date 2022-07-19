28d3aa5ddd793314ddcf92bacef1bf80fe4fa700
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-19 14:47:56 -0700

    alx

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module contains empty class Square """
+
+
+class Square:
+    """ This is an empty class. """
+    def __init__(self, size=0):
+        if  isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
