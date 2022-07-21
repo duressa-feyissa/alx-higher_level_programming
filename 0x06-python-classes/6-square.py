633892698c0334749e56cfc859336b42f83ce8d1
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-21 09:33:19 -0700

    alx

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,9 +36,8 @@ class Square:
     def position(self, value):
         if isinstance(value, tuple) and len(value) == 2:
             if isinstance(value[0], int) and isinstance(value[1], int):
-                self.__position = value
-            else:
-                pass
+                if value[0] >= 0 and value[1] >= 0:
+                    self.__position = value
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
 
