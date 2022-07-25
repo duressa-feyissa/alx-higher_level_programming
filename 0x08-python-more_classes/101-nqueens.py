974652256aef2a0b5383822fed636e1e2cea0eaf
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-25 08:27:21 -0700

    alx

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+
+if len(sys.argv) != 2:
+	print("Usage: nqueens N")
+	sys.exit(1)
+
+if sys.argv[1].isdigit() is False:
+	print("N must be a number")
+	sys.exit(1)
+
+num = int(sys.argv[1])
+if num < 4:
+	print("N must be at least 4")
+	sys.exit(1)
