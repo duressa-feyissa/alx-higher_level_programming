0606cbf0011fbf6e3577c8e1d3b4b8b5d48930d0
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-27 13:20:58 -0700

    alx

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,6 +6,14 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """
+        say_my_name - prints My name is <first name> <last name>.
+        Args:
+            first_name: first name
+            last_name: last name
+        Returns:
+            void
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
