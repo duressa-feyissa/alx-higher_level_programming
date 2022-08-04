6f603ba827e6a6c3becad0d7bb82078aa2c6f49e
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-08-03 20:14:11 -0700

    alx

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
 
     try:
         arg_list = load_from_json_file(filename)
-    except:
+    except FileNotFoundError:
         arg_list = []
 
     for arg in sys.argv[1:]:
