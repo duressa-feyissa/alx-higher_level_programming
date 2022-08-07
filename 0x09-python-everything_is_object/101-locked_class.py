6c4dc40b05a96be4a7bd4510b2b22431fb88b2a1
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-08-07 11:57:36 -0700

    alx

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""
+""" 101-locked_class.py
 """
 
 
 class LockedClass:
     """
+    no class or object attribute, that prevents the
+    user from dynamically creating new instance attributes
     """
     __slots__ = ['first_name']
