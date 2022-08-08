2f74f04289c6b8672aba39fd3be06a3ee71b90b1
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-08-08 16:32:05 -0700

    alx

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
+"""
+contains a class Base.
+"""
 import json
 
 
 class Base():
+    """
+    base class for the entire project.
+    """
 
     __nb_objects = 0
     def __init__(self, id=None):
+        """
+        Initializes the class attributes.
+        """
         if id is not None:
             self.id =id
         else:
@@ -14,6 +23,9 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """
+        returns JSON string repr of list_dictionaries
+        """
         if len(list_dictionaries) == 0:
             return []
         else:
