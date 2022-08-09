ad8cc99393f996ff55a6acd1e0b761a4dd6b712d
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-08-08 19:07:45 -0700

    alx

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# base.py
 """
 contains a class Base.
 """
@@ -8,6 +9,9 @@ import json
 class Base:
     """
     base class for the entire project.
+
+    Attributes:
+    __nb_objects (int): The number of instantiated Bases.
     """
 
     __nb_objects = 0
@@ -32,6 +36,61 @@ class Base:
         Args:
             list_dictionaries (list): A list of dictionaries.
         """
-        if list_dictionaries == [] or list_dictionaries is None:
+        if len(list_dictionaries) or list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write objects to a file.
+        Args:
+            list_objs (list): A list
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                hold = [i.to_dictionary() for i in list_objs]
+                jsonfile.write(Base.to_json_string(new))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the deserialization of a JSON string.
+        Args:
+            json_string (str): A JSON str representation of a list of dicts.
+        Returns:
+            If json_string is None or empty - an empty list.
+            Otherwise - the Python list represented by json_string.
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """create a rectange/ object
+        """
+
+        if not dictionary and dictionary == {}:
+            return
+
+        if cls.__name__ == 'Rectangle':
+            obj = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            obj = cls(1)
+        obj.update(**dictionary)
+        return obj
+
+    @classmethod
+    def load_from_file(cls):
+        """Loads from file
+        """
+        try:
+            with open(cls.__name__ + ".json", 'r') as json_file:
+                objs = Base.from_json_string(json_file.read())
+                return [cls.create(**d) for d in objs]
+        except IOError:
+            return []
+
