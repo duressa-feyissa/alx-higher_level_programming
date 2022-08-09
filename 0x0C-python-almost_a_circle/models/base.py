c364b1424ac1ef0849de4ef1e7ebd632fb1430b8
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-08-08 18:55:09 -0700

    alx'

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,10 +28,10 @@ class Base:
     def to_json_string(list_dictionaries):
         """
         returns JSON string repr of list_dictionaries
+
         Args:
             list_dictionaries (list): A list of dictionaries.
         """
-        if len(list_dictionaries) == 0 or list_dictionaries is None:
+        if list_dictionaries == [] or list_dictionaries is None:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
