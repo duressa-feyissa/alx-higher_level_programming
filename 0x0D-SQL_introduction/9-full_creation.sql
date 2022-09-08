42743726e021a97af6789900913ba0e963f98db5
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-09-08 05:28:32 -0700

    alx

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- A script that creates a table
+
+CREATE TABLE IF NOT EXISTS second_table(
+       id INT,
+       name VARCHAR(256),
+       score INT
+);
+INSERT INTO second_table(id, name, score)
+value(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
