6d40e822ec0de31c5498421f9d53d9259da78152
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-09-08 05:19:59 -0700

    alx

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- a script that inserts a new row in the table
 
-INSERT INTO hbtn_0c_0.first_table
+INSERT INTO first_table(id, name)
 VALUES(89, "Best School");
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- A script that displays the number of records
+
+SELECT COUNT(ID) FROM first_table;
