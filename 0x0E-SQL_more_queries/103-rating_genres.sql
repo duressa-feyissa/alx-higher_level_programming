d49c6562ac0df3cb09c5028ca6bd3d5a240aae6a
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-09-10 12:15:33 -0700

    alx

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,13 @@
+-- Each record should display: tv_genres.name - rating sum
+-- Results must be sorted in descending order by their rating
+
+SELECT name, SUM(rate) as rating
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres
+ON tv_genres.id = genre_id
+JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY name
+ORDER BY rating DESC;
