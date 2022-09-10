c604dd65c8953ac0889888dafa7bc8c55d6a3f09
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-09-10 10:59:30 -0700

    alx'

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Each record should display:
 -- <TV Show genre> - <Number of shows linked to this genre>
 
-SELECT name AS genre, COUNT(tv_show_genres.show_id)
+SELECT name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
