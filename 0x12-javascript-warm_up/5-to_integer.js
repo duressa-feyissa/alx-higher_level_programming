dd5177c9b1dbc7e1d1cfad9a129e75a8121a1605
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-10-03 17:51:12 -0700

    alx

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const args = process.argv;
 if (args[2] !== undefined) {
-  if ( /^\d+\.\d+$|^\d+$/.test(args[2])) {
-    x = parseInt(args[2])
-    console.log('My number:',x);
-  }  else {
+  if (/^\d+\.\d+$|^\d+$/.test(args[2])) {
+    const x = parseInt(args[2]);
+    console.log('My number:', x);
+  } else {
     console.log('Not a number');
   }
 } else {
