7bd2bd73baae09df5057c2beb271cc85461ffa66
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-10-03 18:34:36 -0700

    alx'

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 const args = process.argv;
 if (isNaN(args[2])) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   const len = parseInt(args[2]);
   for (let i = 0; i < len; i++) {
-    console.log('C is fun');
+    let x = '';
+    for (let j = 0; j < len; j++) {
+      x += 'X';
+    }
+    console.log(x);
   }
 }
