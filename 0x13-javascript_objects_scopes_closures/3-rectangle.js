e386c9f12f552a8f1440adcbd0061016a2370fc9
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-10-05 17:48:51 -0700

    alx

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.height = h;
+      this.width = w;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let x = '';
+      for (let j = 0; j < this.width; j++) {
+        x += 'X';
+      }
+      console.log(x);
+    }
+  }
+};
