8af8b6bee1845e2773f6f49d2520cc6059cd2bc5
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-10-07 03:08:34 -0700

    alx

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import MySQLdb
 from sys import argv
+
+
 db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
 cursor = db.cursor()
 cursor.execute("SELECT * FROM states ORDER BY id;")
