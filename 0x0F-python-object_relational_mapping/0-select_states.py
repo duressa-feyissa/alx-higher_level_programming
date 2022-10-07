cf28895370cff1f22afdb1007d410bbcc7c6c1b5
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-10-07 03:10:59 -0700

    alx

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,9 +3,10 @@ import MySQLdb
 from sys import argv
 
 
-db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states ORDER BY id;")
-m = cursor.fetchall()
-for i in m:
-    print(i)
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id;")
+    m = cursor.fetchall()
+    for i in m:
+        print(i)
