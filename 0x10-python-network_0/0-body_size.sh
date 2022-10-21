92955b6943419b54904d0742a33ed0333a09d9db
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-10-21 04:28:08 -0700

    alx

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL
-curl -sI "$1" | grep -oiE 'Content-Length: [0-9]+' | cut -d ' ' -f2
+curl -sI "$1" | grep -h 'Content-Length:' | cut -d ' ' -f2
