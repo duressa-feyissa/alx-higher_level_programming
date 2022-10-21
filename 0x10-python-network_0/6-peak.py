3cda5a93dbe037f89075e61b9ecaa9dfb72d3e9e
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-10-20 18:01:37 -0700

    alx

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,5 +5,5 @@
 def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted integers."""
     if list_of_integers:
-        sorted(list_of_integers, reverse=True)
-        return list_of_integers[0]
+        list_of_integers.sort()
+        return list_of_integers[-1]
