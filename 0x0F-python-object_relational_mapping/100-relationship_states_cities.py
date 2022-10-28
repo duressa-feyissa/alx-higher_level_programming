9c10a3d829db6c026be001679e13047987623b15
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-10-28 02:41:38 -0700

    alx

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -4,6 +4,7 @@
     from the database hbtn_0e_6_usa
 """
 
+from sqlalchemy.engine.url import URL
 from relationship_city import City
 from relationship_state import Base, State
 from sqlalchemy.orm import sessionmaker
@@ -11,13 +12,25 @@ from sqlalchemy import create_engine
 import sys
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    from sqlalchemy.engine import create_engine
+    from sqlalchemy.engine.url import URL
+    from sqlalchemy.orm import Session
+    from relationship_city import City
+    from relationship_state import Base, State
+    from sys import argv
+
+    db = {'drivername': 'mysql+mysqldb',
+          'host': 'localhost',
+          'port': '3306',
+          'username': argv[1],
+          'password': argv[2],
+          'database': argv[3]}
+
+    url = URL(**db)
+    engine = create_engine(url, pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
+    session = Session(engine)
     city = City(name='San Francisco')
     state = State(name='California', cities=[city])
 
