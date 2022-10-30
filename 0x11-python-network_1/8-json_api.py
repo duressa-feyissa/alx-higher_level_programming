bf6a89089412a2cc202ff4e6bdbea36be2fb8610
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-10-30 02:05:06 -0700

    alx'

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -22,5 +22,5 @@ if __name__ == '__main__':
                 print('[{}] {}'.format(out['id'], out['name']))
             else:
                 print('No result')
-        except requests.exceptions.JSONDecodeError:
+        except requests.exceptions.JSONDecodeError as e:
             print('Not a valid JSON')
