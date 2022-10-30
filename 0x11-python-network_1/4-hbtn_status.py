d36cbe0b01933e92917cafb0e3975afccc705f95
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-10-30 00:36:35 -0700

    alx

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@
 import requests
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     response = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
