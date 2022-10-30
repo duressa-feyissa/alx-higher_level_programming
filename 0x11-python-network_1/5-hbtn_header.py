6213ed2e4e0223eb0fd34536c05040e506a6d8f9
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-10-30 00:47:51 -0700

    alx

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,10 +3,14 @@
     sends a request to the URL and displays the value of
     the variable X-Request-Id in the response header
 """
-import requests
-from sys import argv
 
 
 if __name__ == "__main__":
-    res = requests.get(argv[1])
-    print(res.headers['X-Request-Id'])
+    import requests
+    from sys import argv
+
+    try:
+        req = requests.get(argv[1])
+        print(req.headers['X-Request-Id'])
+    except KeyError:
+        pass
