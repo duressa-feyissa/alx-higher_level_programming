de470682c85cc0c792cd70ac04114b30638dffac
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-10-30 02:37:00 -0700

    alx

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" commits on a given GitHub repository.
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    req = requests.get(url)
+    commits = req.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
