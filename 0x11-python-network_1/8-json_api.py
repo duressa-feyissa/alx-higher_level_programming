75c8215473fa2eefcaf7b17959d6aaf2997bf533
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-10-30 01:51:59 -0700

    alx

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+script to take in a URL input, sends a request to the URL, display response
+"""
+
+
+if __name__ == '__main__':
+    from sys import argv
+    import requests
+
+    url = "http://0.0.0.0:5000/search_user"
+    value = {'q': ""}
+    try:
+        value['q'] = argv[1]
+    except IndexError:
+        print('No result')
+    else:
+        req = requests.post(url, data=value)
+        try:
+            out = req.json()
+            if out:
+                print('[{}] {}'.format(out['id'], out['name']))
+            else:
+                print('No result')
+        except:
+            print('Not a valid JSON')
