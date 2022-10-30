46795332abd489201901615ff2af4ab64a8bebfb
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-10-30 00:26:04 -0700

    alx

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,7 +9,7 @@ import sys
 
 
 if __name__ == "__main__":
-    req = urllib.request.urlopen(sys.argv[1])
+    req = urllib.request.Request(sys.argv[1])
     try:
         with urllib.request.urlopen(req) as res:
             html = res.read()
